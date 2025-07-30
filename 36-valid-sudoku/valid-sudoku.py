621da5cbc7252e8691f8503804d0336aac9d081a
Neetha10 <ns6411@nyu.edu>
2025-07-29 18:42:43 -0700

    Time: 7 ms (36.71%) | Memory: 17.8 MB (69.51%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,48 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(9):
+            seen=set()
+            for j in range(9):
+                val=board[i][j]
+                if val !='.':
+                    if val in seen:
+                        return False
+                    seen.add(val)
+        for j in range(9):
+            seen=set()
+            for i in range(9):
+                val=board[i][j]
+                if val!='.':
+                    if val in seen:
+                        return False
+                    seen.add(val)
+        for start_row in [0,3,6]:
+            for start_col in [0,3,6]:
+                seen=set()
+                for i in range(3):
+                    for j in range(3):
+                        val_row=start_row+i
+                        val_col=start_col+j
+                        val=board[val_row][val_col]
+                        if val!='.':
+                            if val in seen:
+                                return False
+                            seen.add(val)
+        return True
+                        
+                            
+                
+
+
+
+               
+                
+
+        return True
+               
+        
+
+
+
+     
+        
