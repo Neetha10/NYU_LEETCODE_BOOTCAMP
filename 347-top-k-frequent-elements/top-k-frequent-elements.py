d05cf958e28bafeab97b94123fcc7357c25bfd45
Neetha10 <ns6411@nyu.edu>
2025-08-28 16:09:38 -0700

    Time: 7 ms (50.33%) | Memory: 21.3 MB (61.95%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,16 +1,23 @@
+from collections import Counter
+import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        output=[]
         count={}
+        heap=[]
         for numbers in nums:
             if numbers not in count:
                 count[numbers]=1
             else:
                 count[numbers]+=1
-        sorted_count=sorted(count.items(),key=lambda item: item[1],reverse=True)
-        for keys in sorted_count:
-            output.append(keys[0])
-        return output[:k]
+        for num , freq in count.items():
+            heapq.heappush(heap,(freq,num))
+            if len(heap)>k:
+                heapq.heappop(heap)
+        result=[]
+        for freq,num in heap:
+            result.append(num)
+        return result
+        
         
 
         
