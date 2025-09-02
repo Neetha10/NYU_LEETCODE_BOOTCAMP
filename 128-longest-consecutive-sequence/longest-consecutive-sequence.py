45c10ce20f361915950c95c12701195d4e5f591d
Neetha10 <ns6411@nyu.edu>
2025-09-02 12:32:39 -0700

    Time: 41 ms (92.46%) | Memory: 34.2 MB (51.30%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+      hashi=set(nums)
+      longest=1
+      if not nums:
+        return 0
+      for num in hashi:
+        if num-1 not in hashi:
+            length=1
+            current=num
+
+            while current+1 in hashi:
+                current+=1
+                length+=1
+
+            longest=max(longest,length)
+      return longest
+        
+      
+
+
+
+    
+
+
+        
+        
